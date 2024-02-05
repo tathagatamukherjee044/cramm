e54ae7517b597a5ea2c33ccfd5200eab317c30b8
tathagatamukherjee044 <tathagata.mukherjee044@gmail.com>
2024-02-05 23:51:19 +0530

    figuring out deploymnet might take multiple commits

--- client/straightAceClient/src/app/_store/config.ts
+++ client/straightAceClient/src/app/_store/config.ts
@@ -2,7 +2,7 @@ import { environment } from "src/environments/environment";
 
 export const  config = {
     googleClientId : '90832106606-pqtgjjtb6bs0u290d95hbf3ndsc0h1in.apps.googleusercontent.com',
-    googleOauthRedirectUrl : 'http://localhost:8080/auth/oauth/google',
+    googleOauthRedirectUrl : `${environment.apiUrl}/auth/oauth/google`,
 
     api : {
         //login
