d0315f63483a68d951e8f1244eec09d272d57ed7
tathagatamukherjee044 <tathagata.mukherjee044@gmail.com>
2024-02-08 02:03:26 +0530

    css changes

--- client/straightAceClient/src/app/home/home.page.ts
+++ client/straightAceClient/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent ,IonRouterOutlet ,IonFooter, IonApp, IonTabBar, IonTabs, IonIcon, IonTabButton } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { IonHeader, IonToolbar, IonTitle, IonContent ,IonRouterOutlet ,IonFooter, IonApp, IonTabBar, IonTabs, IonIcon, IonTabButton, NavController, IonCol, IonRow } from '@ionic/angular/standalone';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -8,15 +9,38 @@ import { environment } from 'src/environments/environment';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonFooter, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent,RouterOutlet, IonApp,IonTabBar,IonTabs,IonIcon,IonTabButton,RouterLink],
+  imports: [RouterOutlet,RouterLink, IonicModule],
 })
 export class HomePage {
-  constructor() {}
+  constructor(
+    private nav : NavController
+  ) {}
 
   array = [1,2,3,4,5,7,3,4,4,4455,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]
+
+  menuItems = [
+    {
+      link : '/learn',
+      display : 'Learn',
+      icon : "book-outline"
+    },
+    {
+      link : '/profile',
+      display : 'Profile',
+      icon: 'person-circle-outline'
+    }
+  ]
 
   ngOnInit() {
     console.log(environment.apiUrl);
     
+  }
+
+  show() {
+    console.log(environment.apiUrl);
+  }
+
+  route(route : string) {
+    this.nav.navigateRoot(route)
   }
 }
