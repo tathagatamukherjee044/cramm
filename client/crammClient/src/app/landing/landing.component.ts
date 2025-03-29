d7248c806ef5389db327edab367550af68f94e11
Tathagata Mukherjee <tathagatamukherjee044@gmail.com>
2025-03-29 16:43:23 +0530

    Refactor privacy and terms components to use SCSS for styling, enhance landing page layout, and remove inline styles

--- client/crammClient/src/app/landing/landing.component.ts
+++ client/crammClient/src/app/landing/landing.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-landing',
     templateUrl: './landing.component.html',
+    styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent {
 
